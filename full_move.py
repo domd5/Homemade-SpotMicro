f430becec2be329c000332934c09ca3df727f693
domd5 <dominic.mohan@gmail.com>
2026-06-02 14:33:17 -0500

    change stand order

--- full_move.py
+++ full_move.py
@@ -30,16 +30,17 @@ def stand():
     kit.servo[3].angle = 90
     kit.servo[6].angle = 90
     kit.servo[9].angle = 90
-    #mid
-    kit.servo[1].angle = 35
-    kit.servo[4].angle = 130
-    kit.servo[7].angle = 40
-    kit.servo[10].angle = 130
     #bottom
     kit.servo[2].angle = 60
     kit.servo[5].angle = 110
     kit.servo[8].angle = 60
     kit.servo[11].angle = 130
+    #mid
+    kit.servo[1].angle = 35
+    kit.servo[4].angle = 130
+    kit.servo[7].angle = 40
+    kit.servo[10].angle = 130
+    
     stand = True
 
 while end:
